b40d23925c2e90643c6274e4959e7e0fd0821674
ocsoares <casoares016@gmail.com>
2023-04-25 21:56:55 -0300

    feat: add cooldown to send notification

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,29 @@
+const notificationCooldown = 20 * 1000; // 20 seconds
+
 chrome.runtime.onConnect.addListener((port) => {
     if (port.name === 'content-script') {
-        port.onMessage.addListener((message) => {
+        port.onMessage.addListener(async (message) => {
             const { sendNotification, mentionedBy } = message;
 
+            const { lastNotification } = await chrome.storage.local.get(
+                'lastNotification',
+            );
+
+            const nextNotification = lastNotification + notificationCooldown;
+
+            const currentTime = Date.now();
+
             if (sendNotification) {
-                chrome.notifications.create({
-                    type: 'basic',
-                    iconUrl: 'icons/mentioned-icon_16.png',
-                    title: 'Twitch Mention Notifier',
-                    message: `Você foi marcado por ${mentionedBy}`,
-                });
+                if (currentTime > nextNotification) {
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: 'icons/mentioned-icon_16.png',
+                        title: 'Twitch Mention Notifier',
+                        message: `Você foi marcado por ${mentionedBy}`,
+                    });
+
+                    chrome.storage.local.set({ lastNotification: Date.now() });
+                }
             }
         });
     }
