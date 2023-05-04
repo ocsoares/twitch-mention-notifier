790daf58664a487ba0915e396de18e885ae9249c
ocsoares <casoares016@gmail.com>
2023-05-03 21:21:46 -0300

    feat: prevent to send notifications to diff tabs

--- src/background.ts
+++ src/background.ts
@@ -6,6 +6,22 @@ chrome.runtime.onConnect.addListener((port) => {
             const { sendNotification, mentionedInChannel, mentionedBy, badge } =
                 message;
 
+            // To prevent the extension from sending notifications to different tabs at the same time
+            await chrome.storage.local.set({
+                lastActiveTabId: port.sender.tab.id,
+            });
+
+            const { lastActiveTabId } = await chrome.storage.local.get(
+                'lastActiveTabId',
+            );
+
+            // Convert the lastActiveTabId to an array to get all tabs at the same time
+            // The lastActiveTabId will always be the same, but with multiple tabs open at the same time,
+            // each tab will send an ID, so these IDs will be added to this array
+            let lastActiveTabIdArray: number[] = [];
+
+            lastActiveTabIdArray.push(lastActiveTabId);
+
             const { lastNotification } = await chrome.storage.local.get(
                 'lastNotification',
             );
@@ -16,54 +32,64 @@ chrome.runtime.onConnect.addListener((port) => {
 
             const currentTime = Date.now();
 
-            if (sendNotification) {
+            if (
+                sendNotification &&
+                lastActiveTabIdArray[0] === port.sender.tab.id // [0] will always be lastActiveTabId !
+            ) {
                 if (currentTime > nextNotification) {
-                    chrome.notifications.create(
-                        {
-                            type: 'basic',
-                            iconUrl: 'icons/mentioned-icon_16.png',
-                            title: 'Twitch Mention Notifier',
-                            message: `You were mentioned by ${badge}${mentionedBy} in channel "${mentionedInChannel}" !`,
-                            buttons: [
-                                {
-                                    title: 'Open Twitch Channel',
-                                },
-                            ],
-                        },
-                        (notificationId) => {
-                            // Open the mentioned Twitch channel when click on the notification window
-                            chrome.notifications.onClicked.addListener(
-                                (clickedNotificationId) => {
-                                    if (
-                                        clickedNotificationId === notificationId
-                                    ) {
-                                        chrome.tabs.create({
-                                            url: `https://www.twitch.tv/${mentionedInChannel}`,
-                                        });
-                                    }
-                                },
-                            );
+                    try {
+                        chrome.notifications.create(
+                            {
+                                type: 'basic',
+                                iconUrl: 'icons/mentioned-icon_16.png',
+                                title: 'Twitch Mention Notifier',
+                                message: `You were mentioned by ${badge}${mentionedBy} in channel "${mentionedInChannel}" !`,
+                                buttons: [
+                                    {
+                                        title: 'Open Twitch Channel',
+                                    },
+                                ],
+                            },
+                            (notificationId) => {
+                                // Open the mentioned Twitch channel when click on the notification window
+                                chrome.notifications.onClicked.addListener(
+                                    (clickedNotificationId) => {
+                                        if (
+                                            clickedNotificationId ===
+                                            notificationId
+                                        ) {
+                                            chrome.tabs.create({
+                                                url: `https://www.twitch.tv/${mentionedInChannel}`,
+                                            });
+                                        }
+                                    },
+                                );
 
-                            // Open the mentioned Twitch channel when click on the notification button
-                            chrome.notifications.onButtonClicked.addListener(
-                                (clickedNotificationId, buttonIndex) => {
-                                    if (
-                                        clickedNotificationId ===
-                                            notificationId &&
-                                        buttonIndex === 0
-                                    ) {
-                                        chrome.tabs.create({
-                                            url: `https://www.twitch.tv/${mentionedInChannel}`,
-                                        });
-                                    }
-                                },
-                            );
-                        },
-                    );
+                                // Open the mentioned Twitch channel when click on the notification button
+                                chrome.notifications.onButtonClicked.addListener(
+                                    (clickedNotificationId, buttonIndex) => {
+                                        if (
+                                            clickedNotificationId ===
+                                                notificationId &&
+                                            buttonIndex === 0
+                                        ) {
+                                            chrome.tabs.create({
+                                                url: `https://www.twitch.tv/${mentionedInChannel}`,
+                                            });
+                                        }
+                                    },
+                                );
+                            },
+                        );
 
-                    await chrome.storage.local.set({
-                        lastNotification: Date.now(),
-                    });
+                        await chrome.storage.local.set({
+                            lastNotification: Date.now(),
+                        });
+
+                        lastActiveTabIdArray = [];
+                    } catch (error) {
+                        console.log('NOTIFICATION NOT SENT', error);
+                    }
                 }
             }
         });
