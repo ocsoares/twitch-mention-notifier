eab8fa30b8b71d83f62b15d363e57c8fa6dc3e95
ocsoares <casoares016@gmail.com>
2024-12-28 12:06:36 -0300

    refactor: Twitch API URL fixed in code (remove supabase call)

--- src/services/check-if-channel-exists.service.ts
+++ src/services/check-if-channel-exists.service.ts
@@ -1,24 +1,11 @@
 import axios from 'axios';
 
 export class CheckIfChannelExists {
-    private static async twitchAPIUrl(): Promise<string> {
-        const twitchAPIUrl = await axios.get(
-            'https://dnebukteuwaxxkrfmhow.supabase.co/storage/v1/object/sign/services/twitch-api-url.json?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJzZXJ2aWNlcy90d2l0Y2gtYXBpLXVybC5qc29uIiwiaWF0IjoxNjkzMjYyNjAwLCJleHAiOjIwMDg4Mzg2MDB9.V7-ij6bgjePO1KKyW3LWa0xLYN4dYx9B3jnxFdIvNjg&t=2023-08-28T22%3A43%3A23.148Z',
-            {
-                headers: {
-                    'Cache-Control': 'no-cache',
-                },
-            },
-        );
-
-        return twitchAPIUrl.data.url;
-    }
+    private static readonly TWITCH_API_URL = 'https://twitch-api-gold.vercel.app/twitch/api/search-channel/'
 
     public static async execute(channel: string): Promise<boolean | undefined> {
         try {
-            const url = await CheckIfChannelExists.twitchAPIUrl();
-
-            const isValidChannel = await axios.get(`${url}${channel}`);
+            const isValidChannel = await axios.get(`${this.TWITCH_API_URL}${channel}`);
 
             try {
                 if (isValidChannel.data.data[0].display_name.length) {
